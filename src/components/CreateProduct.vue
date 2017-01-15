e8e0bbebc98f4817b9323a7301499ff5955404fc
Dan Ross <danross826@gmail.com>
2017-01-14 20:33:50 -0600

    added the create a product page

--- src/components/CreateProduct.vue
+++ src/components/CreateProduct.vue
@@ -4,18 +4,65 @@
         <h2>Fill Out the Form Below</h2>
         <table>
             <tr>
-                <td colspan="6"><input name="name" placeholder="Name" v-model="newItem.name"/></td>
+                <th>Name</th>
+                <td><input name="name"/></td>
             </tr>
             <tr>
-                <td colspan="6"><input name="price" placeholder="Price" v-model="newItem.price"/></td>
+                <th>Price [USD]</th>
+                <td><input name="price"/></td>
             </tr>
             <tr>
-                <td colspan="6"><input name="details" type="text" placeholder="Details" v-model="newItem.details"/></td>
+                <th>Quantity</th>
+                <td><input type="number" name="quantity"/></td>
             </tr>
             <tr>
-                <button v-on:click="addItem()">Submit</button>
+                <th>Short Description</th>
+                <td><textarea name="short_description"></textarea></td>
+            </tr>
+            <tr>
+                <th>Detailed Description</th>
+                <td><textarea name="long_description"></textarea></td>
+            </tr>
+            <tr>
+                <th>Image</th>
+                <td><input type="file" name="product_image"/></td>
+            </tr>
+            <tr>
+                <th colspan="2">Details</th>
+            </tr>
+            <tr>
+                <th>SKU</th>
+                <td><input name="sku"/></td>
+            </tr>
+            <tr>
+                <th>UPC</th>
+                <td><input name="upc"/></td>
+            </tr>
+            <tr>
+                <th>Manufacturer</th>
+                <td><input name="manufacturer"/></td>
+            </tr>
+            <tr>
+                <th>Category</th>
+                <td><input name="category"/></td>
+            </tr>
+            <tr>
+                <th colspan="2">Shipping Information</th>
+            </tr>
+            <tr>
+                <th>Weight</th>
+                <td><input name="weight"/></td>
+            </tr>
+            <tr>
+                <th>Dimensions</th>
+                <td>
+                    <input name="length" placeholder="length"/>
+                    <input name="width" placeholder="width"/>
+                    <input name="depth" placeholder="depth"/>
+                </td>
             </tr>
         </table>
+        <input class="button" type="submit" value="Save Product"/>
     </div>
 </template>
 <style>
