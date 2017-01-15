c0295ee70887cedc63430f2b7c6f171d4bf0f2c5
Michael Main <Michael Main>
2017-01-15 17:48:41 -0600

    Added pagination

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <h1>{{shop.name}}</h1>
+    <paginator :page='page' :pageSize='pageSize' :routeName='"shop"' :params='{shopId: shopId}' />
     <products :shopId='shopId' :page='page' :pageSize='pageSize'></products>
+    <paginator :page='page' :pageSize='pageSize' :routeName='"shop"' :params='{shopId: shopId}' />
   </div>
 </template>
 
 <script>
+  import Paginator from './Paginator.vue'
   import Products from './Products.vue'
   import config from '../config.js'
 
@@ -17,25 +20,28 @@
       }
     },
     components: {
-      'products': Products
+      'products': Products,
+      'paginator': Paginator
     },
     created: function() {
-      this.page = this.$route.query.page;
-      this.pageSize = this.$route.query.pageSize;
-      this.shopId = this.$route.params.shopId;
-
-      if (this.page == undefined) {
-        this.page = 1;
-      }
-
-      if (this.pageSize == undefined) {
-        this.pageSize = 12;
-      }
-
       this.getShop();
+    },
+    watch: {
+      '$route': 'getShop'
     },
     methods: {
       getShop: function() {
+        this.page = this.$route.query.page;
+        this.pageSize = this.$route.query.pageSize;
+        this.shopId = this.$route.params.shopId;
+
+        if (this.page == undefined) {
+          this.page = 1;
+        }
+
+        if (this.pageSize == undefined) {
+          this.pageSize = 12;
+        }
         this.$http.get(config.baseUrl + '/trinary-inventory/v1/shop/' + this.shopId)
           .then((response) => {
             this.shop = response.data
